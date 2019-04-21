fea680854b7b6526937459b1bee0e1a9fe9a28ee
Alisher Zakir <a3zakir@gmail.com>
2019-04-21 14:25:12 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,5 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colors)
-  colors.each.with_index(1) do |color, i|
-    print "#{color[0].upcase}: #{color}"
-    print ", " if i != colors.length
-  end
-    print "\n"
-    return "red"
-  
+  print "#{colors[0][0].upcase}: #{colors[0]}, "
 end
