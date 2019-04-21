4137b8efc530a4d25b965401691a5650f22660da
Alisher Zakir <a3zakir@gmail.com>
2019-04-21 14:25:34 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,4 +2,7 @@
 
 def display_rainbow(colors)
   print "#{colors[0][0].upcase}: #{colors[0]}, "
+  print "#{colors[1][0].upcase}: #{colors[1]}, "
+  print "#{colors[2][0].upcase}: #{colors[2]}, "
+  print "#{colors[3][0].upcase}: #{colors[3]}, "
 end
